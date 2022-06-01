2d1ccd30cabe54eb82640e23e40d98c8c4051aa7
intensum <106609211+intensum@users.noreply.github.com>
2022-06-01 11:00:05 +0200

    Add files via upload

--- SAC/sacgaugeled/box/box.js
+++ SAC/sacgaugeled/box/box.js
@@ -9,8 +9,8 @@
         </style>       
     `;
 
-    const pubnubjs = "http://bo41:81/SAC/sacgaugeled/box/pubnub.4.21.7.min.js";
-    const gaugejs = "http://bo41:81/SAC/sacgaugeled/box/t.js";
+    const pubnubjs = "https://intensum.github.io/sac.github.io/SAC/sacgaugeled/box/pubnub.4.21.7.min.js";
+    const gaugejs = "https://intensum.github.io/sac.github.io/SAC/sacgaugeled/box/t.js";
 
     let googleloaderjs = document.createElement("script");
     googleloaderjs.src = "https://www.gstatic.com/charts/loader.js";
