e47a231103c96d542b41bc509a557292d585baa8
intensum <106609211+intensum@users.noreply.github.com>
2022-06-08 16:38:29 +0200

    Add files via upload

--- SAC/sacgaugeled5/box/main.js
+++ SAC/sacgaugeled5/box/main.js
@@ -34,88 +34,49 @@ var getScriptPromisify = (src) => {
 		await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
   
 		const chart = echarts.init(this._root)
+		var data = [];
+		for (let i = 0; i <= 360; i++) {
+		  let t = (i / 180) * Math.PI;
+		  let r = Math.sin(2 * t) * Math.cos(2 * t);
+		  data.push([r, i]);
+		}
 		const option = {
-			series: [
-			  {
-				type: 'gauge',
-				startAngle: 180,
-				endAngle: 0,
-				min: 0,
-				max: 1,
-				splitNumber: 8,
-				axisLine: {
-				  lineStyle: {
-					width: 6,
-					color: [
-					  [0.25, '#FF6E76'],
-					  [0.5, '#FDDD60'],
-					  [0.75, '#58D9F9'],
-					  [1, '#7CFFB2']
-					]
-				  }
-				},
-				pointer: {
-				  icon: 'path://M12.8,0.7l12,40.1H0.7L12.8,0.7z',
-				  length: '12%',
-				  width: 20,
-				  offsetCenter: [0, '-60%'],
-				  itemStyle: {
-					color: 'auto'
-				  }
-				},
-				axisTick: {
-				  length: 12,
-				  lineStyle: {
-					color: 'auto',
-					width: 2
-				  }
-				},
-				splitLine: {
-				  length: 20,
-				  lineStyle: {
-					color: 'auto',
-					width: 5
-				  }
-				},
-				axisLabel: {
-				  color: '#464646',
-				  fontSize: 20,
-				  distance: -60,
-				  formatter: function (value) {
-					if (value === 0.875) {
-					  return 'A';
-					} else if (value === 0.625) {
-					  return 'B';
-					} else if (value === 0.375) {
-					  return 'C';
-					} else if (value === 0.125) {
-					  return 'D';
-					}
-					return '';
-				  }
-				},
-				title: {
-				  offsetCenter: [0, '-20%'],
-				  fontSize: 30
-				},
-				detail: {
-				  fontSize: 50,
-				  offsetCenter: [0, '0%'],
-				  valueAnimation: true,
-				  formatter: function (value) {
-					return Math.round(value * 100) + '分';
-				  },
-				  color: 'auto'
-				},
-				data: [
-				  {
-					value: 0.7,
-					name: 'Grade Rating'
-				  }
-				]
-			  }
-			]
-		  };
+		  title: {
+			text: 'Lasch Text title'
+		  },
+		  legend: {
+			data: ['line']
+		  },
+		  polar: {
+			center: ['50%', '54%']
+		  },
+		  tooltip: {
+			trigger: 'axis',
+			axisPointer: {
+			  type: 'cross'
+			}
+		  },
+		  angleAxis: {
+			type: 'value',
+			startAngle: 0
+		  },
+		  radiusAxis: {
+			min: 0
+		  },
+		  series: [
+			{
+			  coordinateSystem: 'polar',
+			  name: 'line',
+			  type: 'line',
+			  showSymbol: false,
+			  data: data
+			}
+		  ],
+		  animationDuration: 2000
+		};
+		
+		option && myChart.setOption(option);
+		
 		chart.setOption(option)
 	  }
 	}
