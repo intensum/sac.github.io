9e5d96bb39f2d89dac902e6c7dc68b559a3374af
intensum <106609211+intensum@users.noreply.github.com>
2023-03-28 12:06:22 +0200

    Add files via upload

--- SAC/sacgaugeled5/box/box.js
+++ SAC/sacgaugeled5/box/box.js
@@ -1,242 +1,124 @@
-(function() {
-    let shadowRoot;
+(function() { 
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<style>
+		:host {
+			border-radius: 10px;
+			border-width: 2px;
+			border-color: black;
+			border-style: solid;
+			display: block;
+		} 
 
-    let ArMbit = [], Ar = [], ArData = [], ArOptions = [], ArChart = [], ArPubNub = [];
+		body {
+		  background: #fff;
+		}
+		
+		.metric {
+		  padding: 10%;
+		}
+		
+		.metric svg {
+		  max-width: 100%;
+		}
+		
+		.metric path {
+		  stroke-width: 75;
+		  stroke: #ecf0f1;
+		  fill: none;
+		}
+		
+		.metric text {
+		  font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;
+		}
+		
+		.metric.participation path.data-arc {
+		  stroke: #27ae60;
+		}
+		
+		.metric.participation text {
+		  fill: #27ae60;
+		}		
+		</style>
+		
+		<div class="container">
+		  <div class="row">
+		    <div class="col-md-4 col-sm-4">
+		      <div class="metric participation" data-ratio=".95">
+		        <svg viewBox="0 0 1000 500">
+			        <path d="M 950 500 A 450 450 0 0 0 50 500"></path>
+					<text class='percentage' text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">0%</text>
+					<text class='title' text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal"></text>
+  	            </svg>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	`;
 
-    let template = document.createElement("template");
-    template.innerHTML = `
-        <style type="text/css"> 
-        </style>       
-    `;
+	class Box extends HTMLElement {
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			
+			this.$style = shadowRoot.querySelector('style');			
+			this.$svg = shadowRoot.querySelector('svg');
+			
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
+			
+			this._props = {};
+		}
+		
+		render(val, info, color) {
+			var val1 = val * 0.01;
+			var x = this.svg_circle_arc_path(500, 500, 450, -90, val1 * 180.0 - 90);
+			var rounded = Math.round( val * 10 ) / 10;
 
-    const pubnubjs = "https://intensum.github.io/sac.github.io/SAC/sacgaugeled5/box/pubnub.4.21.7.min.js";
-    const gaugejs = "https://intensum.github.io/sac.github.io/SAC/sacgaugeled5/box/t.js";
+			
+			if(rounded >=0 && rounded <=100) {
+				this.$style.innerHTML = ':host {border-radius: 10px;border-width: 2px;border-color: black;border-style: solid;display: block;}.body {background: #fff;}.metric {padding: 10%;}.metric svg {max-width: 100%;}.metric path {stroke-width: 75;stroke: #ecf0f1;fill: none;}.metric text {font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;}.metric.participation path.data-arc {stroke: ' + color + ';}.metric.participation text {fill: ' + color + ';}';
+				this.$svg.innerHTML = '<path d="M 950 500 A 450 450 0 0 0 50 500"></path><text class="percentage" text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">' + rounded + '%</text><text class="title" text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal">' + info + '</text><path d="' + x + '" class="data-arc"></path>"';
+			}
+		}
+		  
+		polar_to_cartesian(cx, cy, radius, angle) {
+		    var radians;
+		    radians = (angle - 90) * Math.PI / 180.0;
+		    return [Math.round((cx + radius * Math.cos(radians)) * 100) / 100, Math.round((cy + radius * Math.sin(radians)) * 100) / 100];
+		}
+		
+		svg_circle_arc_path(x, y, radius, start_angle, end_angle) {
+		    var end_xy, start_xy;
+		    start_xy = this.polar_to_cartesian(x, y, radius, end_angle);
+		    end_xy = this.polar_to_cartesian(x, y, radius, start_angle);
+		    return "M " + start_xy[0] + " " + start_xy[1] + " A " + radius + " " + radius + " 0 0 0 " + end_xy[0] + " " + end_xy[1];
+		  };
+		  
 
-    let googleloaderjs = document.createElement("script");
-    googleloaderjs.src = "https://www.gstatic.com/charts/loader.js";
+		onCustomWidgetBeforeUpdate(changedProperties) {
+			this._props = { ...this._props, ...changedProperties };
+		}
 
-    function loadScript(src, callback) {
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = src;
-        script.addEventListener("load", callback);
-        shadowRoot.appendChild(script);
-    };
-
-    // Google Chart
-    function GoogleChart(divstr, text, value, firsttime) {
-        google.setOnLoadCallback(function() {
-            drawChart(divstr, text, value, firsttime);
-        });
-    };
-
-    function drawChart(divstr, text, value, firsttime) {
-        if (firsttime === 0) {
-
-            const uuid = PubNub.generateUUID();
-            const pubnub = new PubNub({
-                publishKey: "",
-                subscribeKey: "",
-                uuid: uuid
-            });
-
-            var foundIndex = ArData.findIndex(x => x.id == text);
-            if (foundIndex === -1) {
-
-                var data = google.visualization.arrayToDataTable([
-                    ['Label', 'Value'],
-                    [text, value]
-                ]);
-
-                ArData.push({
-                    'id': text,
-                    'data': data
-                });
-
-                var options = {
-                    width: 600,
-                    height: 240,
-                    redFrom: 80,
-                    redTo: 100,
-                    yellowFrom: 55,
-                    yellowTo: 90,
-                    greenFrom: 0,
-                    greenTo: 55,
-                    minorTicks: 5
-                };
-
-                ArOptions.push({
-                    'id': text,
-                    'options': options
-                });
-
-                var chart = new google.visualization.Gauge(divstr);
-                chart.draw(data, options);
-
-                ArChart.push({
-                    'id': text,
-                    'chart': chart
-                });
-
-                ArPubNub.push({
-                    'id': text,
-                    'pubnub': pubnub,
-                    'uuid': uuid
-                });
-            }
-
-        } else {
-            var foundIndex = ArData.findIndex(x => x.id == text);
-
-            ArPubNub[foundIndex].pubnub.publish({
-                channel: "pubnub_onboarding_channel",
-                message: {
-                    "sender": ArPubNub[foundIndex].uuid,
-                    "content": value
-                }
-            }, function(status, response) {
-                console.log(status);
-                console.log(response);
-            });
-
-            ArData[foundIndex].data.setValue(0, 1, parseInt(value));
-            ArChart[foundIndex].chart.draw(ArData[foundIndex].data, ArOptions[foundIndex].options);
-        }
-    };
-
-    function Draw(Ar, firsttime) {
-        for (var i = 0; i < Ar.length; i++) {
-            GoogleChart(Ar[i].div, Ar[i].id, 0, firsttime);
-        }
-    };
-
-    class Box extends HTMLElement {
-        constructor() {
-            console.log("constructor");
-            super();
-            shadowRoot = this.attachShadow({
-                mode: "open"
-            });
-
-            shadowRoot.appendChild(googleloaderjs);
-            shadowRoot.appendChild(template.content.cloneNode(true));
-
-            this._firstConnection = 0;
-
-            this.addEventListener("click", event => {
-                console.log('click');
-                var event = new Event("onClick");
-                this.dispatchEvent(event);
-            });
-            this._props = {};
-        }
-
-        connectedCallback() {
-            console.log("connectedCallback");
-        }
-
-        onCustomWidgetBeforeUpdate(changedProperties) {
-            console.log("onCustomWidgetBeforeUpdate");
-            this._props = {
-                ...this._props,
-                ...changedProperties
-            };
-        }
-
-        onCustomWidgetAfterUpdate(changedProperties) {
-            console.log("onCustomWidgetAfterUpdate");
-            console.log(changedProperties);
-
-            if ("value" in changedProperties) {
-                console.log("value:" + changedProperties["value"]);
-                this.$value = changedProperties["value"];
-            }
-
-            if ("title" in changedProperties) {
-                console.log("title:" + changedProperties["title"]);
-                this.$title = changedProperties["title"];
-            }
-
-            if ("subtitle" in changedProperties) {
-                console.log("subtitle:" + changedProperties["subtitle"]);
-                this.$subtitle = changedProperties["subtitle"];
-            }
-
-            if ("min" in changedProperties) {
-                console.log("min:" + changedProperties["min"]);
-                this.$min = changedProperties["min"];
-            }
-
-            if ("max" in changedProperties) {
-                console.log("max:" + changedProperties["max"]);
-                this.$max = changedProperties["max"];
-            }
-
-            var that = this;
-
-            if (this._firstConnection === 0) {
-                const div = document.createElement('div');
-                let divid = changedProperties.widgetName;
-                this._tagContainer = divid;
-                div.innerHTML = '<div id="chart_div' + divid + '"></div>';
-                shadowRoot.appendChild(div);
-
-
-                var mapcanvas_divstr = shadowRoot.getElementById('chart_div' + divid);
-                console.log(mapcanvas_divstr);
-                this._divid = mapcanvas_divstr;
-
-                Ar.push({
-                    'id': divid,
-                    'div': mapcanvas_divstr,
-                    'value': this.$value,
-                    'title': this.$title,
-                    'subtitle': this.$subtitle,
-                    'min': this.$min,
-                    'max': this.$max
-                });
-
-                loadScript(pubnubjs, function() {
-                    console.log("Load:" + pubnubjs);
-
-                    loadScript(gaugejs, function() {
-                        console.log("Load:" + gaugejs);
-                        Draw(Ar, that._firstConnection);
-                        that._firstConnection = 1;
-                    });
-                });
-
-            } else {
-                var id = this.$value.split("|")[0];
-                console.log("id: " + id);
-
-                var value = this.$value.split("|")[1];
-                console.log("value:" + value);
-
-                var title = this.$title;
-                console.log("title: " + title);
-
-                var subtitle = this.$subtitle;
-                console.log("subtitle: " + subtitle);
-
-                var min = this.$min;
-                console.log("min: " + min);
-
-                var max = this.$max;
-                console.log("max: " + max);
-
-
-                if (value !== "") {
-                    var foundIndex = Ar.findIndex(x => x.id == id);
-                    console.log("foundIndex: " + foundIndex);
-
-                    if (foundIndex !== -1) {
-                        drawChart(Ar[foundIndex].div, id, parseInt(value), this._firstConnection);
-                    }
-                }
-            }
-        }
-    }
-
-    customElements.define("com-fd-gaugeled", Box);
+		onCustomWidgetAfterUpdate(changedProperties) {
+			if ("value" in changedProperties) {
+				this.$value = changedProperties["value"];
+			}
+			
+			if ("info" in changedProperties) {
+				this.$info = changedProperties["info"];
+			}
+			
+			if ("color" in changedProperties) {
+				this.$color = changedProperties["color"];
+			}
+			
+			this.render(this.$value, this.$info, this.$color);
+		}
+	}
+	
+	customElements.define("com-demo-gauge-lasch5", Box);
 })();
