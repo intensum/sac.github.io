d501f50d9d37774fe0b21006f0609e09a62b042b
intensum <106609211+intensum@users.noreply.github.com>
2023-03-29 15:20:35 +0200

    v0.0.5

--- SAC/Exercice3/webcomponent.js
+++ SAC/Exercice3/webcomponent.js
@@ -1,7 +1,40 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>Hello Exercice3</h1>
+    <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['gauge']});
+      google.charts.setOnLoadCallback(drawGauge);
+  
+      var gaugeOptions = {min: 0, max: 280, yellowFrom: 200, yellowTo: 250,
+        redFrom: 250, redTo: 280, minorTicks: 5};
+      var gauge;
+  
+      function drawGauge() {
+        gaugeData = new google.visualization.DataTable();
+        gaugeData.addColumn('number', 'Engine');
+        gaugeData.addColumn('number', 'Torpedo');
+        gaugeData.addRows(2);
+        gaugeData.setCell(0, 0, 120);
+        gaugeData.setCell(0, 1, 80);
+  
+        gauge = new google.visualization.Gauge(document.getElementById('gauge_div'));
+        gauge.draw(gaugeData, gaugeOptions);
+      }
+  
+      function changeTemp(dir) {
+        gaugeData.setValue(0, 0, gaugeData.getValue(0, 0) + dir * 25);
+        gaugeData.setValue(0, 1, gaugeData.getValue(0, 1) + dir * 20);
+        gauge.draw(gaugeData, gaugeOptions);
+      }
+    </script>
+   </head>
+   <body>
+    <div id="gauge_div" style="width:280px; height: 140px;"></div>
+    <input type="button" value="Go Faster" onclick="changeTemp(1)" />
+    <input type="button" value="Slow down" onclick="changeTemp(-1)" />
+   </body>
     `;
 
     customElements.define('com-sap-sample-exercice3', class exercice3 extends HTMLElement {
