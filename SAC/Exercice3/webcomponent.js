4ce0da2b7082dbe3a92bce4bb1fe06d9cc8d09b9
intensum <106609211+intensum@users.noreply.github.com>
2023-03-29 16:07:34 +0200

    v0.0.4

--- SAC/Exercice3/webcomponent.js
+++ SAC/Exercice3/webcomponent.js
@@ -1,41 +1,43 @@
 (function()  {
     let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
-    <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['gauge']});
-      google.charts.setOnLoadCallback(drawGauge);
-  
-      var gaugeOptions = {min: 0, max: 280, yellowFrom: 200, yellowTo: 250,
-        redFrom: 250, redTo: 280, minorTicks: 5};
-      var gauge;
-  
-      function drawGauge() {
-        gaugeData = new google.visualization.DataTable();
-        gaugeData.addColumn('number', 'Engine');
-        gaugeData.addColumn('number', 'Torpedo');
-        gaugeData.addRows(2);
-        gaugeData.setCell(0, 0, 120);
-        gaugeData.setCell(0, 1, 80);
-  
-        gauge = new google.visualization.Gauge(document.getElementById('gauge_div'));
-        gauge.draw(gaugeData, gaugeOptions);
-      }
-  
-      function changeTemp(dir) {
-        gaugeData.setValue(0, 0, gaugeData.getValue(0, 0) + dir * 25);
-        gaugeData.setValue(0, 1, gaugeData.getValue(0, 1) + dir * 20);
-        gauge.draw(gaugeData, gaugeOptions);
-      }
-    </script>
-   </head>
-   <body>
-    <div id="gauge_div" style="width:280px; height: 140px;"></div>
-    <input type="button" value="Go Faster" onclick="changeTemp(1)" />
-    <input type="button" value="Slow down" onclick="changeTemp(-1)" />
-   </body>
-    `;
+    tmpl.innerHTML = `<form method='post' action='https://jsfiddle.net/api/post/library/pure/' target="_blank">
+   <input type='submit' value='Code it yourself on JSFiddle' class='button'/>
+   <input type='hidden' name='title' value='Gauge Chart Example'/>
+   <textarea name='html' style="display:none;">
+&lt;script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"&gt;&lt;/script&gt;
+     &lt;div id=&quot;gauge_div&quot; style=&quot;width:280px; height: 140px;&quot;&gt;&lt;/div&gt;
+  &lt;input type=&quot;button&quot; value=&quot;Go Faster&quot; onclick=&quot;changeTemp(1)&quot; /&gt;
+  &lt;input type=&quot;button&quot; value=&quot;Slow down&quot; onclick=&quot;changeTemp(-1)&quot; /&gt;
+   </textarea>
+   <textarea name='js' style="display:none;">    google.charts.load(&#39;current&#39;, {&#39;packages&#39;:[&#39;gauge&#39;]});
+    google.charts.setOnLoadCallback(drawGauge);
+
+    var gaugeOptions = {min: 0, max: 280, yellowFrom: 200, yellowTo: 250,
+      redFrom: 250, redTo: 280, minorTicks: 5};
+    var gauge;
+
+    function drawGauge() {
+      gaugeData = new google.visualization.DataTable();
+      gaugeData.addColumn(&#39;number&#39;, &#39;Engine&#39;);
+      gaugeData.addColumn(&#39;number&#39;, &#39;Torpedo&#39;);
+      gaugeData.addRows(2);
+      gaugeData.setCell(0, 0, 120);
+      gaugeData.setCell(0, 1, 80);
+
+      gauge = new google.visualization.Gauge(document.getElementById(&#39;gauge_div&#39;));
+      gauge.draw(gaugeData, gaugeOptions);
+    }
+
+    function changeTemp(dir) {
+      gaugeData.setValue(0, 0, gaugeData.getValue(0, 0) + dir * 25);
+      gaugeData.setValue(0, 1, gaugeData.getValue(0, 1) + dir * 20);
+      gauge.draw(gaugeData, gaugeOptions);
+    }</textarea>
+   <input type='hidden' name='css' value=''/>
+   <input type='hidden' name='description' value='An example of a Google Gauge Chart, with buttons that control it'/>
+   <input type='hidden' name='dtd' value='html 5'/>
+   <input type='hidden' name='wrap' value='b'/>
+ </form>`;
 
     customElements.define('com-sap-sample-exercice3', class exercice3 extends HTMLElement {
 
