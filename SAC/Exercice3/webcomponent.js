b54b94081ecfbc7b124d3b2bc66629306bc6d815
intensum <106609211+intensum@users.noreply.github.com>
2023-03-30 11:37:40 +0200

    v000015

--- SAC/Exercice3/webcomponent.js
+++ SAC/Exercice3/webcomponent.js
@@ -4,7 +4,7 @@
     <h1>Hello Super Mini World 2</h1>
     google.charts.load('current', {'packages':['gauge']});
             google.charts.setOnLoadCallback(drawGauge);
-         
+          
             var gaugeOptions = {min: 0, max: 280, yellowFrom: 200, yellowTo: 250,
               redFrom: 250, redTo: 280, minorTicks: 5};
             var gauge;
