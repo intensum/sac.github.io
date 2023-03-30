812664e58403b55c50f4a2b59057540e5158e2d9
intensum <106609211+intensum@users.noreply.github.com>
2023-03-30 11:38:35 +0200

    v000017

--- SAC/Exercice3/webcomponent.js
+++ SAC/Exercice3/webcomponent.js
@@ -1,31 +1,7 @@
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `<h1>Hello Super Mini World</h1>
-    <h1>Hello Super Mini World 2</h1>
-    google.charts.load('current', {'packages':['gauge']});
-            google.charts.setOnLoadCallback(drawGauge);
-          
-            var gaugeOptions = {min: 0, max: 280, yellowFrom: 200, yellowTo: 250,
-              redFrom: 250, redTo: 280, minorTicks: 5};
-            var gauge;
-        
-            function drawGauge() {
-              gaugeData = new google.visualization.DataTable();
-              gaugeData.addColumn('number', 'Engine');
-              gaugeData.addColumn('number', 'Torpedo');
-              gaugeData.addRows(2);
-              gaugeData.setCell(0, 0, 120);
-              gaugeData.setCell(0, 1, 80);
-        
-              gauge = new google.visualization.Gauge(document.getElementById('gauge_div'));
-              gauge.draw(gaugeData, gaugeOptions);
-            }
-        
-            function changeTemp(dir) {
-              gaugeData.setValue(0, 0, gaugeData.getValue(0, 0) + dir * 25);
-              gaugeData.setValue(0, 1, gaugeData.getValue(0, 1) + dir * 20);
-              gauge.draw(gaugeData, gaugeOptions);
-    `;
+    <h1>Hello Super Mini World 2</h1>    `;
     console.log("scripting en cours");
     customElements.define('com-sap-sample-exercice3', class exercice3 extends HTMLElement {
  
