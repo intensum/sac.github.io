bf08b5d176fab61bccc33f470b1bc82adee5ce20
intensum <106609211+intensum@users.noreply.github.com>
2023-03-30 14:18:28 +0200

    v1

--- SAC/Exercice3/webcomponent.js
+++ SAC/Exercice3/webcomponent.js
@@ -1,6 +1,7 @@
 (function()  {
     let tmpl = document.createElement('template');
-    tmpl.innerHTML = `<h1>Hello Super Mini World 3</h1>`;
+    tmpl.innerHTML = `<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <div id="chart_div2" style="width: 400px; height: 120px;"></div>`;
     console.log("scripting en cours");
     customElements.define('com-sap-sample-exercice3', class exercice3 extends HTMLElement {
  
@@ -11,6 +12,43 @@
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
 		}
+
+        google.charts.load('current', {'packages':['gauge']});
+        google.charts.setOnLoadCallback(drawChart);
+  
+        function drawChart() {
+  
+          var data = google.visualization.arrayToDataTable([
+            ['Label', 'Value'],
+            ['Memory', 80],
+            ['CPU', 55],
+            ['Network', 68]
+          ]);
+  
+          var options = {
+            width: 400, height: 120,
+            redFrom: 90, redTo: 100,
+            yellowFrom:75, yellowTo: 90,
+            minorTicks: 5
+          };
+  
+          var chart = new google.visualization.Gauge(document.getElementById('chart_div2'));
+  
+          chart.draw(data, options);
+  
+          setInterval(function() {
+            data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+            chart.draw(data, options);
+          }, 13000);
+          setInterval(function() {
+            data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+            chart.draw(data, options);
+          }, 5000);
+          setInterval(function() {
+            data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+            chart.draw(data, options);
+          }, 26000);
+        }
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
