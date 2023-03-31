e752aa533895f9a21d848bf47a38c8fb057ab01e
intensum <106609211+intensum@users.noreply.github.com>
2023-03-31 13:38:07 +0200

    v13

--- SAC/Gauge/SACGoogleGauge/box/box.js
+++ SAC/Gauge/SACGoogleGauge/box/box.js
@@ -26,9 +26,7 @@
     // Google Chart
     function GoogleChart(divstr, text, value, firsttime) {
         google.setOnLoadCallback(function() {
-            //text="essai3";
-            
-            drawChart(divstr, text, value, firsttime);
+               drawChart(divstr, text, value, firsttime);
         });
     };
 
